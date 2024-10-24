4297c36bfa4cf3e206005beacaed1eb0cd7d48eb
uds <udovenko@bluewin.ch>
2024-10-24 10:37:57 +0200

    refactor: Update physics core_test.cljs to use vec2d

--- test/tails/physics/core_test.cljs
+++ test/tails/physics/core_test.cljs
@@ -5,17 +5,17 @@
 
 (deftest test-integration-step
   (testing "integration-step updates rigid body correctly"
-    (let [initial-rigid-body {:position         (v/vector 0 0)
+    (let [initial-rigid-body {:position         (v/vec2d 0 0)
                               :orientation      0
-                              :velocity         (v/vector 1 0)
+                              :velocity         (v/vec2d 1 0)
                               :angular-velocity 0
-                              :force            (v/vector 0 0)
+                              :force            (v/vec2d 0 0)
                               :torque           0
                               :inverse-mass     1
                               :inverse-inertia  1
-                              :linear-damping   0.1
-                              :angular-damping  0.1}
+                              :linear-damping   1
+                              :angular-damping  0}
           delta-time 1
           updated-rigid-body (integration-step initial-rigid-body delta-time)]
-      (is (= (:position updated-rigid-body) (v/vector 0.9 0)))
+      (is (= (:position updated-rigid-body) (v/vec2d 0.97 0)))
       (is (= (:orientation updated-rigid-body) 0)))))
