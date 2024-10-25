27b9cffa5b8832031640b07496b3504f81598f2d
uds <udovenko@bluewin.ch>
2024-10-25 11:36:26 +0200

    test: Update collision test to use namespaced function reference

--- test/tails/physics/collision_test.cljs
+++ test/tails/physics/collision_test.cljs
@@ -1,20 +1,20 @@
 (ns tails.physics.collision-test
   (:require [cljs.test :refer-macros [deftest is testing]]
-            [tails.physics.collision :refer [circle-vs-circle?]]
+            [tails.physics.collision :as c]
             [tails.math.vector2d :as v]))
 
 (deftest test-circle-vs-circle?
   (testing "No collision when circles are apart"
-    (is (nil? (circle-vs-circle? (v/vec2d 0 0) 1 (v/vec2d 3 0) 1))))
+    (is (nil? (#'c/circle-vs-circle? (v/vec2d 0 0) 1 (v/vec2d 3 0) 1))))
 
   (testing "Collision when circles overlap"
-    (let [collision-info (circle-vs-circle? (v/vec2d 0 0) 2 (v/vec2d 3 0) 2)]
+    (let [collision-info (#'c/circle-vs-circle? (v/vec2d 0 0) 2 (v/vec2d 3 0) 2)]
       (is (not (nil? collision-info)))
       (is (= (:penetration collision-info) 1))
       (is (= (:normal collision-info) (v/normalize (v/vec2d 3 0))))))
 
   (testing "Collision when circles are at the same position"
-    (let [collision-info (circle-vs-circle? (v/vec2d 0 0) 1 (v/vec2d 0 0) 1)]
+    (let [collision-info (#'c/circle-vs-circle? (v/vec2d 0 0) 1 (v/vec2d 0 0) 1)]
       (is (not (nil? collision-info)))
       (is (= (:penetration collision-info) 1))
-      (is (= (:normal collision-info) (v/right))))))
+      (is (= (:normal collision-info) v/right)))))
