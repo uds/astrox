629215e1a069ac62992bec0a7e48f05b69b1c4d0
uds <udovenko@bluewin.ch>
2024-10-25 11:33:21 +0200

    refactor: Improve circle collision detection logic and handle edge case

--- src/tails/physics/collision.cljs
+++ src/tails/physics/collision.cljs
@@ -2,14 +2,17 @@
   "Collision detection and resolution."
   (:require [tails.math.vector2d :as v]))
 
-(defn circle-vs-circle?
+(defn- circle-vs-circle?
   "Detects collision between two circles and returns collision info with penetration depth and normal vector."
   [pos1 radius1 pos2 radius2]
   (let [vector (v/sub pos2 pos1)
         radius-sum (+ radius1 radius2)]
     (if (< (v/length-squared vector) (* radius-sum radius-sum))
-      (let [distance (v/length vector)
-            normal (if (zero? distance) (v/vec2d 1 0) (v/normalize vector))]
-        {:penetration (- radius-sum distance)
-         :normal normal})
+      (let [distance (v/length vector)]
+        ;; if circles are on the same position, choose some random but consistent values
+        (if (zero? distance)
+          {:penetration radius1
+           :normal v/right}
+          {:penetration (- radius-sum distance)
+           :normal (v/normalize vector)}))
       nil)))
