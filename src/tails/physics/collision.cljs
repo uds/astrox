287fbd333afd9ae1fdc5173488956b1f0a246ee2
uds <udovenko@bluewin.ch>
2024-10-25 11:03:51 +0200

    refactor: Simplify collision detection logic in circle-vs-circle function

--- src/tails/physics/collision.cljs
+++ src/tails/physics/collision.cljs
@@ -5,12 +5,10 @@
 (defn circle-vs-circle?
   "Detects collision between two circles and returns collision info with penetration depth and normal vector."
   [pos1 radius1 pos2 radius2]
-  (let [distance-vector (v/sub pos2 pos1)
-        distance-squared (v/length-squared distance-vector)
-        radius-sum (+ radius1 radius2)
-        radius-sum-squared (* radius-sum radius-sum)]
-    (if (< distance-squared radius-sum-squared)
-      (let [distance (Math/sqrt distance-squared)]
-        {:penetration-depth (- radius-sum distance)
-       :normal-vector (v/normalize distance-vector)}
+  (let [vector (v/sub pos2 pos1)
+        radius-sum (+ radius1 radius2)]
+    (if (< (v/length-squared vector) (* radius-sum radius-sum))
+      (let [distance (v/length vector)]
+        {:penetration (- radius-sum distance)
+         :normal (v/normalize vector)})
       nil)))
